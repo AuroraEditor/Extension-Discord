7bf195a79f606da73c8db76a2b243d35c5b5588d
Wesley de Groot <email@wesleydegroot.nl>
2022-10-07 17:04:49 +0200

    Extension now sets discord status

--- Discord.swift
+++ Discord.swift
@@ -9,31 +9,35 @@ import Foundation
 import AEExtensionKit
 
 public class DiscordExtension: ExtensionInterface {
+    let rpc = SwordRPC(appId: "1023938668349640734")
     var api: ExtensionAPI
 
     init(api: ExtensionAPI) {
+        rpc.connect()
         self.api = api
         print("Hello from Discord EXT: \(api)!")
     }
 
-    public func didOpen(workspace: String, file: String, contents: String) {
+    public func didOpen(workspace: String, file: String, contents: Data) {
+        print("didOpen(workspace: String, file: String, contents: Data)", workspace, file)
         setDiscordStatusTo(project: workspace, custom: file)
     }
 
     func setDiscordStatusTo(project: String, custom: String) {
-        let rpc = SwordRPC(appId: "1023938668349640734")
-        rpc.connect()
+        rpc.clearPresence()
 
+        let pURL = NSURL(string: project)?.lastPathComponent
+        let cURL = NSURL(string: custom)?.lastPathComponent
         var presence = RichPresence()
-            presence.assets.largeImage = "auroraeditor"
-            presence.details = project
-            presence.state = custom
+            presence.assets.largeImage = "AuroraEditor"
+            presence.details = pURL ?? project
+            presence.state = cURL ?? custom
 
         rpc.setPresence(presence)
     }
 }
 
-@objc(DiscordExtension)
+@objc(DiscordExtensionBuilder)
 public class DiscordExtensionBuilder: ExtensionBuilder {
     public override func build(withAPI api: ExtensionAPI) -> ExtensionInterface {
         return DiscordExtension(api: api)
